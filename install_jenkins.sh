8f6df66248e0304f46e9ab4950185b500b441efe
joshking1 <88409463+joshking1@users.noreply.github.com>
2022-02-25 21:49:09 -0500

    Update install_jenkins.sh

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -41,17 +41,14 @@ sudo service docker start
 sudo chkconfig docker on
 
 echo "Install Jenkins"
-sudo yum update –y
+sudo yum update -y
+sudo amazon-linux-extras install java-openjdk11 -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-sudo yum upgrade
-yum install jenkins java-1.8.0-openjdk-devel -y
+sudo yum upgrade -y   
+sudo yum install jenkins -y
 amazon-linux-extras install epel -y
-sudo systemctl start jenkins
 sudo systemctl enable jenkins
+sudo systemctl start jenkins
 sudo systemctl status jenkins
-systemctl daemon-reload
-
-
-
